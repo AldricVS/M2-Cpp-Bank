d021b36c2f8b9f7075e37e63618ac71ed007c700
AldricVS <aldric.silvestre@outlook.fr>
2022-09-12 20:45:00 +0200

    fix cashier serving ghost client

--- src/event/ClientDeparture.cpp
+++ src/event/ClientDeparture.cpp
@@ -16,12 +16,17 @@ void ClientDeparture::execute()
     cout << "departure" << endl;
     _bank->setRealLength(hour());
     delete _client;
+    // delete does not set the _client pointer to null
+    _client = nullptr;
     // manage Cashier depending presence of other clients
     if(_bank->nbClients() == 0){
         _cashier->wait();
     }
     else{
         Client* newClient = _bank->getWaitingClient();
-        _cashier->serve(newClient);
+        if (newClient != nullptr)
+        {
+            _cashier->serve(newClient);
+        }
     }
 }
